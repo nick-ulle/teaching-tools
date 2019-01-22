03ce7489703e2043f8c34b4ecb15ea85ffaa0b44
Nick Ulle <nick.ulle@gmail.com>
2019-01-22 15:42:14 -0800

    Update config.

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from datetime import datetime
 import pytz
 
 # Base URL to git repos. Pick HTTPS or SSH.
-#base_url = "https://github.com/2019-winter-ucdavis-sta141b/"
-base_url = "git@github.com:2019-winter-ucdavis-sta141b/"
+base_url = "https://github.com/2019-winter-ucdavis-sta141b/"
+#base_url = "git@github.com:2019-winter-ucdavis-sta141b/"
 
 year = datetime.now().year
 tzinfo = pytz.timezone("US/Pacific")
